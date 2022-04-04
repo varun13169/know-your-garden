8413c6f42ea7d779e1c0290ded137977bb94c9d8
Varun Kumar <varun@Varun-MacBook-Pro.local>
2022-04-05 01:48:43 +0530

    Implement Counter

--- src/page/quizPage/QuizPage.jsx
+++ src/page/quizPage/QuizPage.jsx
@@ -1,14 +1,22 @@
 import "./quiz-page.css";
 import { Navbar } from "../../components";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useQuiz } from "../../context";
 import { useNavigate, useParams } from "react-router-dom";
 import axios from "axios";
 
 export default function QuizPage() {
+  const [counter, setCounter] = useState(60);
   const { quizState, setQuizState } = useQuiz();
   const { id } = useParams();
   const navigate = useNavigate();
+
+  useEffect(() => {
+    counter > 0 && setTimeout(() => setCounter(counter - 1), 1000);
+    counter == 0 && navigate("/result");
+  }, [counter]);
+
+  console.log(counter);
 
   useEffect(() => {
     let config = {
@@ -39,6 +47,14 @@ export default function QuizPage() {
             <h1 className="question-card__title dui-primary-color dui-util-txt-align-cent">
               Plant Types
             </h1>
+
+            <div className="question-card__counter">
+              <p
+                className={`dui-util-txt-reg dui-util-fw-sbld${
+                  counter <= 10 ? " question-card__counter-text-red " : ""
+                }`}
+              >{`Time Left ${counter}s`}</p>
+            </div>
 
             <p className="question-card__question dui-util-txt-align-cent dui-util-spc-pad-s dui-util-txt-md">
               {quizState.questions[quizState.questionIdx].question}
