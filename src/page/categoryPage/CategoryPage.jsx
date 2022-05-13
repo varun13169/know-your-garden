6b631d70ec5bb5816fa6aab798dd9753d8b9b3ec
Varun Kumar <varun@Varun-MacBook-Pro.local>
2022-05-14 02:19:21 +0530

    Add Sidebar

--- src/page/categoryPage/CategoryPage.jsx
+++ src/page/categoryPage/CategoryPage.jsx
@@ -3,7 +3,7 @@ import { useEffect } from "react";
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import { quizeCategories } from "../../backend/db/quizCategory";
-import { Navbar } from "../../components";
+import { Navbar, Sidebar } from "../../components";
 import { useCategory } from "../../context";
 import "./category-page.css";
 // import categoryPageStyles from "./category-page.modules.css";
@@ -36,6 +36,10 @@ export default function CategoryPage() {
         <Navbar></Navbar>
       </section>
 
+      <section className="category-page-namespace page-sidebar">
+        <Sidebar></Sidebar>
+      </section>
+
       <section className="category-page-namespace page-main">
         <main
           className={
@@ -46,120 +50,7 @@ export default function CategoryPage() {
             Quiz Category
           </h1>
 
-          <fieldset className="category-page-namespace quiz-category-filter">
-            <legend>
-              <span>Sort By Quiz Count</span>
-            </legend>
-            <div>
-              {/* <!-- Radio Button Component Starts --> */}
-              <label className="dui-inp-radio-btn dui-util-txt-sm">
-                Low to High
-                <input
-                  type="radio"
-                  name="sort-by"
-                  checked={categoryState.sortByQuizCount === "LOW_TO_HIGH"}
-                  onChange={() =>
-                    setCategoryState({
-                      type: "SORT_BY_QUIZ_COUNT",
-                      data: { sortByQuizCount: "LOW_TO_HIGH" },
-                    })
-                  }
-                />
-                <span className="dui-inp-radio-btn__checkmark"></span>
-              </label>
-
-              <label className="dui-inp-radio-btn dui-util-txt-sm">
-                High to Low
-                <input
-                  type="radio"
-                  name="sort-by"
-                  checked={categoryState.sortByQuizCount === "HIGH_TO_LOW"}
-                  onChange={() =>
-                    setCategoryState({
-                      type: "SORT_BY_QUIZ_COUNT",
-                      data: { sortByQuizCount: "HIGH_TO_LOW" },
-                    })
-                  }
-                />
-                <span className="dui-inp-radio-btn__checkmark"></span>
-              </label>
-              {/* <!-- Radio Button Component Ends --> */}
-            </div>
-          </fieldset>
-
-          <fieldset className="category-page-namespace quiz-category-filter">
-            <legend>
-              <span>Filter By Quiz Count</span>
-            </legend>
-
-            <div>
-              {/* <!-- Radio Button Component Starts --> */}
-              <label className="dui-inp-radio-btn dui-util-txt-sm">
-                4 Quizzes &amp; above
-                <input
-                  type="radio"
-                  name="rating"
-                  checked={categoryState.filterByQuizCount === 4}
-                  onChange={() =>
-                    setCategoryState({
-                      type: "FILTER_BY_QUIZ_COUNT",
-                      data: { filterByQuizCount: 4 },
-                    })
-                  }
-                />
-                <span className="dui-inp-radio-btn__checkmark"></span>
-              </label>
-
-              <label className="dui-inp-radio-btn dui-util-txt-sm">
-                3 Quizzes &amp; above
-                <input
-                  type="radio"
-                  name="rating"
-                  checked={categoryState.filterByQuizCount === 3}
-                  onChange={() =>
-                    setCategoryState({
-                      type: "FILTER_BY_QUIZ_COUNT",
-                      data: { filterByQuizCount: 3 },
-                    })
-                  }
-                />
-                <span className="dui-inp-radio-btn__checkmark"></span>
-              </label>
-
-              <label className="dui-inp-radio-btn dui-util-txt-sm">
-                2 Quizzes &amp; above
-                <input
-                  type="radio"
-                  name="rating"
-                  checked={categoryState.filterByQuizCount === 2}
-                  onChange={() =>
-                    setCategoryState({
-                      type: "FILTER_BY_QUIZ_COUNT",
-                      data: { filterByQuizCount: 2 },
-                    })
-                  }
-                />
-                <span className="dui-inp-radio-btn__checkmark"></span>
-              </label>
-
-              <label className="dui-inp-radio-btn dui-util-txt-sm">
-                1 Quiz &amp; above
-                <input
-                  type="radio"
-                  name="rating"
-                  checked={categoryState.filterByQuizCount === 1}
-                  onChange={() =>
-                    setCategoryState({
-                      type: "FILTER_BY_QUIZ_COUNT",
-                      data: { filterByQuizCount: 1 },
-                    })
-                  }
-                />
-                <span className="dui-inp-radio-btn__checkmark"></span>
-              </label>
-              {/* <!-- Radio Button Component Ends --> */}
-            </div>
-          </fieldset>
+          {/* Varundev */}
 
           <div className="category-page-namespace category-card-container">
             {categoryState.categoriesToShow.map((category) => {
